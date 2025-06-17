1847288bfc3c3efdb95c9b3288e86a3690b86493
PabloLemosP <pablo.lemos@sandboxquantum.com>
2025-06-17 07:52:03 +0000

    Updated bash

--- make_plots.sh
+++ make_plots.sh
@@ -2,3 +2,6 @@ python plot_affinity.py
 python plot_folder.py
 python plot_posebusters.py
 python plot_potencies.py
+python plot_pocket_clusters.py
+python pocket_confidency.py
+python protein_seq_len_analysis.py
